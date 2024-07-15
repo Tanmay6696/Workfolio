37254c7bf553983bb3f6a77d0081f2a15b46b3cc
Tanmay6696 <tanmaydahale111@gmail.com>
2024-07-15 22:59:20 +0530

    modified

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,3 @@
-
-const express = require('express')
-require('dotenv').config()
-const app = express()
-const port = 4000
-
-app.get('/', (req, res) => {
-  res.send('Hello Word!')
-})
-
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port} and ${process.env.PORT}`)
-})
+require('dotenv').config({path:'./env'})
+import connectDB from '../src/db/index.js'
+connectDB()
---
+++ Backend/src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../Constant";
+
+const connectDB=async()=>{
+    try{
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log("mongodbhost connected",connectionInstance.connection.host);
+    }
+    catch(e)
+    {
+        console.log("error",e);
+        process.exit(1);
+    }
+}
+export default connectDB
