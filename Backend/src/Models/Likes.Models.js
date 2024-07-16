93af566a22001ca406d60c9b74db2db32e38edd3
MAYUR JADHAV <jadhavm45281@gmail.com>
2024-07-16 22:00:14 +0530

    likes

--- Backend/src/Models/Likes.Models.js
+++ Backend/src/Models/Likes.Models.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mongoose from mongoose;
 
 const likesSchema = new mongoose.Schema({
@@ -23,10 +22,3 @@ const likesSchema = new mongoose.Schema({
 },{timestamps: true})
 
 export const Likes = mongoose.model("Likes",likesSchema)
-=======
-import mongoose from "mongoose";
-const LikeSchema=new Schema({
-    
-});
-export const Like=mongoose.model("Like",LikeSchema);
->>>>>>> a43f322a15b8397bd97db6af27980f5af296a011
