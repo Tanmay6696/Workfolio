316c247b4203ccfe1492993cdc0e3f92b991bba0
Tanmay6696 <tanmaydahale111@gmail.com>
2024-08-11 19:14:45 +0530

    Education changes by Tanmay

--- Frontend/workfolio/src/Components/Education.js
+++ Frontend/workfolio/src/Components/Education.js
@@ -2,7 +2,18 @@ import React from 'react'
 import '../Componentcss/Educations.css'
 const Education = () => {
   return (
-    <div className='Education'>Education</div>
+    <div className='Education'>
+      <div className='EducationHeader'>
+        <h1>Education</h1>
+      </div>
+      <div className='EducationDetails'>
+        <div className='clgname'>College Name</div>
+        <div className='Coursedetails'>
+          <span> BE </span>
+          <span> ENTC</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
