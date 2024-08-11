5e234b1d2c0f31c30f62f7143c48a1eddb70e700
Tanmay6696 <tanmaydahale111@gmail.com>
2024-08-11 16:55:22 +0530

    Tanmay Changes

--- Frontend/workfolio/src/Components/UserInfo.js
+++ Frontend/workfolio/src/Components/UserInfo.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+import logo from '../logo.svg';
+
 import '../Componentcss/UserInfo.css';
 const UserInfo = () => {
   return (
     <div className='UserInfo'>
-        <div>pgoto</div>
-        <div>summarys</div>
+        <div className='UserPhoto'>
+          <div className='Userphotos'>
+            <img src={logo} className='Userphotoimg'/>
+          </div>
+          
+        </div>
+        <div className='UserSummary'>
+          
+            <div><h1>Tanmay Vinay dahale</h1></div>
+            <div><h2>Tanmay Vinay dahale</h2></div>
+            <div>Summary SummarySummarySummary Summary Summaryvv Summary Summary Summary </div>
+          
+
+          
+        </div>
     </div>
   )
 }
---
+++ Frontend/workfolio/src/SupplementaryComponenets/Inputs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Inputs = () => {
+  return (
+    <div>
+        <input placeholder="enter the filed"/>
+    </div>
+  )
+}
+
+export default Inputs
