c1c715cbafee477f91c800802b885af40df513c9
MAYUR JADHAV <jadhavm45281@gmail.com>
2024-08-11 20:07:30 +0530

    skills css and userinfo css changes by mayur

--- Frontend/workfolio/src/Components/Skills.js
+++ Frontend/workfolio/src/Components/Skills.js
@@ -1,9 +1,48 @@
-import React from 'react'
-import '../Componentcss/Skills.css';
+import React from "react";
+import "../Componentcss/Skills.css";
 const Skills = () => {
   return (
-    <div className='skills'>Skills</div>
-  )
-}
+    <div className="skills">
+      <div className="skillheader">
+        <h1>Skills</h1>
+      </div>
+      <div className="skilldiv">
+        <div className="allskills">
+          <div className="one">
+            <span>react</span></div>
+           <div className="one">
+            <span>python</span></div>
+           <div className="one">
+            <span>redux</span></div>
+          <div className="one">react</div>
+           <div className="one">
+            <span>python</span></div>
+           <div className="one">
+            <span>redux</span></div>
+          <div className="one">react</div>
+           <div className="one">
+            <span>python</span></div>
+           <div className="one">
+            <span>redux</span></div>
+          <div className="one">react</div>
+           <div className="one">
+            <span>python</span></div>
+           <div className="one">
+            <span>redux</span></div>
+          <div className="one">react</div>
+           <div className="one">
+            <span>python</span></div>
+           <div className="one">
+            <span>redux</span></div>
+          <div className="one">react</div>
+           <div className="one">
+            <span>python</span></div>
+           <div className="one">
+            <span>redux</span></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Skills
+export default Skills;
--- Frontend/workfolio/src/Components/UserInfo.js
+++ Frontend/workfolio/src/Components/UserInfo.js
@@ -4,28 +4,30 @@ import logo from '../logo.svg';
 import '../Componentcss/UserInfo.css';
 const UserInfo = () => {
   return (
-    <div className='UserInfo'>
-        <div className='UserPhoto'>
-          <div className='Userphotos'>
-            <img src={logo} className='Userphotoimg'/>
-          </div>
-          
+    <div className="UserInfo">
+      <div className="UserPhoto">
+        <div className="Userphotos">
+          <img src={logo} className="Userphotoimg" />
         </div>
-        <div className='UserSummary'>
-          
-            <div className='name'><span className='names'>Tanmay Vinay dahale</span></div>
-            <div className='role'><span className='roles'>Tanmay Vinay dahale</span></div>
-            <div className='summary'>
-              <span className='summary-content'>Summary SummarySummarySummary Summary Summaryvv Summary Summary Summary
-              Summary SummarySummarySummary Summary Summaryvv Summary Summary Summary
-              Summary SummarySummarySummary Summary Summaryvv Summary Summary Summary </span>
-              </div>
-          
-
-          
+      </div>
+      <div className="UserSummary">
+        <div className="name">
+          <span className="names">Tanmay Vinay dahale</span>
         </div>
+        <div className="role">
+          <span className="roles">Tanmay Vinay dahale</span>
+        </div>
+        <div className="summary">
+          <span className="summary-content">
+            Summary SummarySummarySummary Summary Summaryvv Summary Summary
+            Summary Summary SummarySummarySummary Summary Summaryvv Summary
+            Summary Summary Summary SummarySummarySummary Summary Summaryvv
+            Summary Summary Summary
+          </span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default UserInfo
