2ed1762bdc5852ecf78f7185d54dfa19b2a8f621
Tanmay6696 <tanmaydahale111@gmail.com>
2024-08-12 09:06:16 +0530

    Latest Changes from Tanmay

--- Frontend/workfolio/src/Components/Experiences.js
+++ Frontend/workfolio/src/Components/Experiences.js
@@ -2,7 +2,23 @@ import React from 'react'
 import '../Componentcss/Experiencess.css'
 const Experiences = () => {
   return (
-    <div className='Experience'>Experiences</div>
+    <div className='Experience'>
+      <div className='ExperiencesHeader'>
+        <h1>Experiences</h1></div>
+      <div className='Experiencemain'>
+        <div className='ExperiencesCompanyName'>
+          <span>Company Name</span>
+        </div>
+        <div>
+          <div className='ExperiencesTitle'>
+            <span>Title</span>
+          </div>
+          <div className='Experiencesfromto'>
+            <span>from to</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
