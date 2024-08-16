5d00bd9e8df2d02b71a32c5b8610e14b35c2ed48
Tanmay6696 <tanmaydahale111@gmail.com>
2024-08-16 18:57:02 +0530

    New changes from Tanmay

--- Frontend/workfolio/src/App.js
+++ Frontend/workfolio/src/App.js
@@ -2,15 +2,23 @@ import Navbar from './Components/Navbar';
 import Video from './Components/Video';
 import Profilesection from './Components/Profilesection';
 import AllComponentfile from './AllComponentfile';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 
 function App() {
+  const [userdata,setuserdata]=useState([]);
   const getdata = async () => {
     try {
-      const response = await axios.get('http://localhost:3000/api/v1/users/getdetails');
-      console.log("response",response.data.message);
+      const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI5MTcwNmMxNC02NWI5LTQ0ZDYtOThjYy04ZWYyZWVlOTRjOTYiLCJlbWFpbCI6ImphbmVkb2VAZXhhbXBsZS5jb20iLCJ1c2VybmFtZSI6ImphbmVkb2UiLCJmdWxsTmFtZSI6IkphbmUgRG9lIiwiaWF0IjoxNzIzODEwMDMwLCJleHAiOjE3MjM4OTY0MzB9.DbwbEreo5i5mafvWfnn4BlFp2iK2EBkhzD-7kt2F7rQ";
+      const response = await axios.post('http://localhost:3000/api/v1/johndoe@example.com/Userdetails',{},{
+        headers:{
+          'Authorization':`${token}`
+        }
+      });
+      
+      setuserdata(response.data);
+      console.log("response from getusera all data ",userdata);
     } catch (e) {
       console.log("Error:", e);
     }
@@ -18,12 +26,17 @@ function App() {
 
   // Use useEffect to call getdata on component mount
   useEffect(() => {
-    console.log("HI");
     
-    getdata(); // Call the function, not invoke it directly
+    
+    getdata(); 
+    console.log("HI  userdata ",userdata.fullName);
+    // Call the function, not invoke it directly
   }, []);
   return (
     <div className="App">
+      
+        <a>{userdata.fullName}</a>
+     
       <AllComponentfile/>
       
     </div>
