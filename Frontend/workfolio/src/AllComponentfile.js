8c1ee34ba25c8060f1afa3557b7f0e732ca434f8
Tanmay6696 <tanmaydahale111@gmail.com>
2024-08-28 23:09:05 +0530

    New Changes

--- Frontend/workfolio/src/AllComponentfile.js
+++ Frontend/workfolio/src/AllComponentfile.js
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './Components/Navbar';
 import Video from './Components/Video';
+import { useDispatch, useSelector } from 'react-redux';
+
 import Profilesection from './Components/Profilesection';
 import './Allcomponentfile.css';
+import { fetchusersdata } from './Store/UserDataSlice';
 
 const AllComponentfile = () => {
+  const dispatch=useDispatch();
+  const allusersdata=useSelector((state)=> state.UserData.allusersdata );
+  const userstatus=useSelector((state)=>state.UserData.status);
+  useEffect(()=>{
+    if(userstatus=='idle'){
+      console.log("hi0");
+      dispatch(fetchusersdata());
+      console.log("userstatus",userstatus);
+      
+    }
+  },[userstatus,dispatch]);
+  let content;
+  if (userstatus === 'succeeded') {
+    content = allusersdata.map((user, index) => (
+      <div className='main' key={index}>
+        <Video user={user} />
+        <Profilesection user={user} />
+      </div>
+    ));
+  } else if (userstatus === 'loading') {
+    content = <div>Loading...</div>;
+  } else if (userstatus === 'failed') {
+    content = <div>Error loading users</div>;
+  }
+  
+   
   return (
+    
     <div className='Allcomponent'>
         <Navbar/>
-        <div className='main'>
-            <Video/>
-            <Profilesection/>
-        </div>
+        {content}
 
     </div>
   )
--- Frontend/workfolio/src/Store/UserDataSlice.js
+++ Frontend/workfolio/src/Store/UserDataSlice.js
@@ -1,17 +1,48 @@
-import {createSlice} from '@reduxjs/toolkit';
-const initialState={
-    userdata:"null"
-};
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import axios from 'axios';
+
+//async to call data
+export const fetchusersdata=createAsyncThunk(
+    'users/fetchusersdata',
+    async()=>{
+        console.log("hi1");
+        
+        const response=await axios.get('http://localhost:3000/api/v1/getotherUserdetails');
+        console.log("hi2" ,response);
+
+        return response.data;
+    }
+)
 const UserDataSlice = createSlice({
     name:'UserData',
-    initialState,
+    initialState:{
+        allusersdata:[],
+        status:'idle',
+        userdata:[],
+        error:null,
+    },
     reducers:{
         setUserdata:(state,action)=>{
             console.log("hi");
             
             state.userdata=action.payload;
         },
-    }
-})
+    },
+    extraReducers:(builder)=>{
+        builder
+        .addCase(fetchusersdata.pending,(state)=>{
+            state.status='loading';
+        })
+        .addCase(fetchusersdata.fulfilled,(state,action)=>{
+            state.status='succeeded';
+            state.allusersdata=action.payload;
+        })
+        .addCase(fetchusersdata.rejected,(state,action)=>{
+            state.status='failed';
+            state.error=action.error.message;
+        });
+
+    },
+});
 export const {setUserdata}=UserDataSlice.actions;
 export  default UserDataSlice.reducer;
