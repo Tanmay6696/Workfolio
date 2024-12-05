4aaa793915e9ea0d56a0f6072a8d5536d381335d
Tanmay6696 <99316889+Tanmay6696@users.noreply.github.com>
2024-12-05 16:14:02 +0530

    Update App.js

--- Backend/src/App.js
+++ Backend/src/App.js
@@ -3,9 +3,7 @@ const app=express()
 import cors from 'cors';
 import UserRouter from './routes/Users.Routes.js'
 import UserDetailsRoutes from './routes/UserDetails.Routes.js'
-app.use(cors({
-    origin: 'http://localhost:3001',
-  }));
+
   
 app.use(express.json());
 
